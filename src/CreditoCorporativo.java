4d7bd94e89664231599dcfeb20c359628e69c7c0
diegourrego567-lgtm <diegourrego567@gmail.com>
2026-02-07 00:42:17 +0000

    correcion

--- src/CreditoCorporativo.java
+++ src/CreditoCorporativo.java
@@ -1,28 +1,28 @@
 public class CreditoCorporativo{
-    private String Id ;
+    private String id;
     private double montoPrestado;
     private double saldoPendiente; 
     private double tasaInteres;
     private boolean esResgioso;
  
     
-    public CreditoCorporativo(String Id,double montoPrestado,double tasaInteres) {
-        this.Id= Id;
+    public CreditoCorporativo(String id,double montoPrestado,double tasaInteres) {
+        this.id= id;
         this.montoPrestado=montoPrestado;
         this.saldoPendiente=montoPrestado;
         this.tasaInteres=tasaInteres;
         this.esResgioso=false;
         
     }
-    public CreditoCorporativo(String Id,double montoPrestado,double tasaInteres,double saldoPendiente) {
-        this.Id= Id;
+    public CreditoCorporativo(String id,double montoPrestado,double tasaInteres,double saldoPendiente) {
+        this.id= id;
         this.montoPrestado= montoPrestado;
         this.saldoPendiente= saldoPendiente;
         this.tasaInteres= tasaInteres;
         this.esResgioso= false;
     }
-    public CreditoCorporativo(String Id,double montoPrestado) {
-        this.Id= Id;
+    public CreditoCorporativo(String id,double montoPrestado) {
+        this.id= id;
         this.montoPrestado=montoPrestado;
         this.saldoPendiente=montoPrestado;
         this.tasaInteres=1.5;
@@ -31,7 +31,7 @@ public class CreditoCorporativo{
 
     public void SetsaldoPendiente(double saldoPendiente){
         if(saldoPendiente< 0){
-            System,out.print("Error");
+            System.out.println("Error");
         }else{
             this.saldoPendiente=saldoPendiente;
         }
